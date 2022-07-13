00d7d3e4d0c5df665c3c8a27dbd6be5b34870914
Chuck28 <38671575+Chuck28@users.noreply.github.com>
2022-07-12 20:52:26 -0400

    Add files via upload

---
+++ Simple Python Calculator.py
@@ -0,0 +1,25 @@
+#Charles Cohen
+#7/12/22
+#Cohen-Calculator.py
+#Create Calculator
+
+operator= input("Please enter: add, subtract, multiply, or divide: ")
+print ("You chose " + str(operator) + ".")
+number1 = input("What is the first number? ")
+number1 = float(number1)
+number2 = float(input("What is the second number? "))
+if operator .lower() == "add":
+        answer = number1 + number2
+        print(number1 , "+",number2 , "==",answer) 
+elif operator .lower() == "subtract":
+        answer = number1 - number2
+        print(number1 , "-", number2 , "==", answer)
+elif operator .lower() == "multiply":
+        answer = number1 * number2
+        print(number1 , "*", number2 , "==", answer)
+elif operator .lower() == "divide":
+        answer = number1 + number2
+        print(number1 , "/", number2 , "==", answer)
+else:
+    print("The option you chose (" + operator + ") is not valid")
+    print("Please try this program again")
